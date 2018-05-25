2da1b8e4968b703c63202dcd0d211cd8bb787437
Daniel Wright <rLinks234@gmail.com>
2018-05-25 13:52:59 -0400

    initial commit for util package

---
+++ util/input/__init__.py
@@ -0,0 +1,36 @@
+
+import os
+
+def __inject_sdl2_win():
+  from platform import machine
+  cpu = machine()
+  lib_base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'lib'))
+
+  if cpu == 'AMD64':
+    lib_path = os.path.join(lib_base_path, 'win_x64', 'SDL2.dll')
+  elif cpu == 'x86':
+    lib_path = os.path.join(lib_base_path, 'win_x86', 'SDL2.dll')
+  else:
+    raise RuntimeError('Unsupported Platform {0}'.format(cpu))
+  os.environ['PYSDL2_DLL_PATH'] = lib_path
+
+
+if os.name == 'nt':
+  if 'PYSDL2_DLL_PATH' not in os.environ:
+    __inject_sdl2_win()
+
+
+from . import event_handler as __event_handler
+
+def register_event(event, callback):
+  """
+  Register a callback for the given event
+  """
+  try:
+    __event_handler._singleton.register_event(event, callback)
+  except Exception as e:
+    print('Caught exception when attempting to register event')
+    print(str(e))
+
+
+from .joystick import Joystick
