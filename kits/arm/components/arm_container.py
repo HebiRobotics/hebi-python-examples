6665bd15644b294de4be91eeca3bc3a7908688fd
Daniel Wright <rLinks234@gmail.com>
2018-05-28 12:04:52 -0400

    add initial teach repeat example

--- kits/arm/components/arm_container.py
+++ kits/arm/components/arm_container.py
@@ -1,2 +1,70 @@
 import numpy as np
 import hebi
+
+
+class ArmContainer(object):
+
+  def __init__(self, group, robot_model):
+    self._group = group
+    self._robot = robot_model
+    self._masses = robot_model.masses
+
+  @property
+  def dof_count(self):
+    return self._robot.dof_count
+
+  @property
+  def group(self):
+    return self._group
+
+  @property
+  def robot(self):
+    return self._robot
+
+  def get_efforts(feedback):
+    """
+    Gets the torques whih approximately balance out the effect
+    of gravity on the arm
+    """
+    gravity = -1.0*feedback[0].accelerometer
+    gravity_norm = np.linalg.norm(gravity)
+    if gravity_norm > 0.0:
+      gravity *= (9.81)/gravity_norm
+    else:
+      return np.zeros(self._robot.dof_count, dtype=np.float64)
+
+    num_dof = self._robot.dof_count
+    num_frames = self._robot.get_frame_count('com')
+    jacobians = self._robot.get_jacobians('com', feedback.position)
+
+    masses = self._masses
+    comp_torque = np.zeros(num_dof, dtype=np.float64)
+    wrench_vec = np.zeros(6, dtype=np.float64)
+
+    for i in range(num_frames):
+      # Set translational part
+      for j in range(3):
+        wrench_vec[j] = -gravity[j] * masses[i]
+      comp_torque += jacobians[i].T * wrench_vec
+
+    return comp_torque
+
+
+def create_3_dof():
+  lookup = hebi.Lookup()
+  arm = lookup.get_group_from_names(['HEBI'], ['base', 'shoulder', 'elbow'])
+
+  if arm is None:
+    print('Could not find arm group')
+    raise RuntimeError()
+
+  model = hebi.robot_model.RobotModel()
+  model.add_actuator('X5-4')
+  model.add_bracket('X5-LightBracket', 'right')
+  model.add_actuator('X5-4')
+  model.add_link('X5', extension=0.18, twist=np.pi)
+  model.add_actuator('X5-4')
+  model.add_link('X5', extension=0.18, twist=0)
+
+  assert arm.size == model.dof_count
+  return ArmContainer(arm, model)
