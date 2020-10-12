e0e57668b43e15c2da20bb391a96a7dbaf6d216d
Daniel Wright <daniel@hebirobotics.com>
2020-10-12 00:27:07 -0400

    Begin converting arm examples to use arm api from hebi-py

--- kits/arm/ex_impedance_control.py
+++ kits/arm/ex_impedance_control.py
@@ -3,28 +3,19 @@
 import hebi
 import numpy as np
 import os
-import sys
 from time import sleep
-
-# ------------------------------------------------------------------------------
-# Add the root folder of the repository to the search path for modules
-root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-sys.path = [root_path] + sys.path
-# ------------------------------------------------------------------------------
-
-
-from util.math_utils import get_grav_comp_efforts, rot2axisangle
 from hebi.util import create_mobile_io
-from util.arm import setup_arm_params
+from hebi import arm as arm_api
 from matplotlib import pyplot as plt
 
-# Set up our mobile io interface
 phone_family = "HEBI"
-phone_name = "mobileIO"
+phone_name   = "mobileIO"
+arm_family   = "Example Arm"
 
 lookup = hebi.Lookup()
 sleep(2)
 
+# Setup Mobile IO
 print('Waiting for Mobile IO device to come online...')
 m = create_mobile_io(lookup, phone_family, phone_name)
 if m is None:
@@ -33,37 +24,33 @@ m.set_button_mode(1, 'momentary')
 m.set_button_mode(2, 'toggle')
 m.update()
 
-arm_family = 'Example Arm'
-arm_name   = '6-DoF + gripper'
-# If you attach a gas spring to the shoulder for extra payload, set this to True
-has_gas_spring = False
+# Setup arm components
+arm = arm_api.create(arm_family,
+                     lookup=lookup,
+                     hrdf_file=os.path.join(os.path.dirname(__file__), 'hrdf', 'A-2085-06G.hrdf'))
+impedance_controller = arm_api.ImpedanceController()
+end_effector = arm_api.Gripper()
 
-group, kin, params = setup_arm_params(arm_name, arm_family, has_gas_spring)
-
-gravity_vec = params.gravity_vec.copy()
-effort_offset = params.effort_offset
-local_dir = params.local_dir
+# Configure arm components
+arm.add_plugin(impedance_controller)
+impedance_controller.set_damper_gains(10, 10, 0, .1, .1, .1)
+impedance_controller.set_spring_gains(500, 500, 0, 5, 5, 5)
+arm.set_end_effector(end_effector)
 
 # Increase feedback frequency since we're calculating velocities at the
 # high level for damping. Going faster can help reduce a little bit of
 # jitter for fast motions, but going slower (100 Hz) also works just fine
 # for most applications.
-group.feedback_frequency = 200.0
+arm.group.feedback_frequency = 200.0
 
 # Double the effort gains from their default values, to make the arm more sensitive for tracking force.
-gains = params.gains
-gains.effort_kp = 2.0 * gains.effort_kp
-group.send_command(gains)
+# TODO
 
 enable_logging = True
 
 # Start background logging 
 if enable_logging:
-  group.start_log('dir', 'logs', mkdirs=True)
-
-# Gravity compensated mode
-cmd = hebi.GroupCommand(group.size)
-fbk = hebi.GroupFeedback(group.size)
+  arm.group.start_log('dir', 'logs', mkdirs=True)
 
 print('Commanded gravity-compensated zero force to the arm.')
 print('  b2 - Toggles an impedance controller on/off:')
@@ -71,124 +58,33 @@ print('          ON  - Apply controller based on current position')
 print('          OFF - Go back to gravity-compensated mode')
 print('  b1 - Exits the demo.')
 
-gains_in_end_effector_frame = True
-damper_gains = np.asarray([10, 10, 0, .1, .1, .1])
-spring_gains = np.asarray([500, 500, 0, 5, 5, 5])
-
-fbk = group.get_next_feedback(reuse_fbk=fbk)
-
-# FIXME: Fix the hellish broadcasting shape issues
-
-# Error terms
-pos_error = np.zeros(fbk.size)
-vel_error = np.zeros(fbk.size)
-
-xyz_error = np.zeros((3, 1))
-rot_error_vec = np.zeros((3, 1))
-
-spring_wrench = np.zeros((6, 1))
-damper_wrench = np.zeros((6, 1))
-
-# Effort components
-impedance_effort = np.zeros((6, 1))
-grav_effort = np.zeros((6, 1))
-
-# Velocity commands
-arm_cmd_joint_angs = fbk.position
-arm_cmd_joint_vels = np.zeros((1, fbk.size))
-
-arm_tip_fk = kin.get_end_effector(arm_cmd_joint_angs)
-J_arm_tip = kin.get_jacobian_end_effector(arm_cmd_joint_angs)
-
-end_effector_XYZ = arm_tip_fk[0:3,3].copy()
-end_effector_rot_mat = arm_tip_fk[0:3,0:3].copy()
-
-
 controller_on = False
 
 # while button 1 is not pressed
 while not m.get_button_state(1):
-  # Update MobileIO state
+
+  # Update impedance controller state
+  impedance_controller.enabled = controller_on
+
+  if not arm.update():
+    print("Failed to update arm")
+    continue
+
   if not m.update():
     print("Failed to get feedback from MobileIO")
     continue
 
-  # Gather sensor data from the arm
-  group.get_next_feedback(reuse_fbk=fbk)
+  arm.send()
 
-  fbk_position = fbk.position
-
-  # Update gravity vector the base module of the arm
-  params.update_gravity(fbk)
-  gravity_vec[:] = -params.gravity_vec
-
-  # Calculate required torques to negate gravity at current position
-  grav_effort = get_grav_comp_efforts(kin, fbk_position, gravity_vec) + effort_offset
-  kin.get_end_effector(fbk_position, output=arm_tip_fk)
-  # If in grav comp mode set led green
-  if not controller_on:
-      m.set_led_color("green")
-      end_effector_XYZ = arm_tip_fk[0:3,3].copy()
-      end_effector_rot_mat = arm_tip_fk[0:3,0:3].copy()
-  
-  if controller_on:
-    # If in impedance mode set led blue
-    m.set_led_color("blue")
-    
-    # Get Updated Forward Kinematics and Jacobians
-    kin.get_jacobian_end_effector(fbk_position, output=J_arm_tip)
-
-    # Calculate Impedence Control Wrenches and Appropraite Joint Torque
-    spring_wrench[:] = 0
-    damper_wrench[:] = 0
-
-    # Linear error is easy
-    xyz_error[0:3] = (end_effector_XYZ - arm_tip_fk[0:3, 3]).reshape(3, 1)
-
-    # Rotational error involves calculating axis-angle from the
-    # resulting error in S03 and providing a torque around that axis.
-    error_rot_mat = end_effector_rot_mat @ arm_tip_fk[0:3, 0:3].T
-    axis, angle = rot2axisangle(error_rot_mat)
-    rot_error_vec[:, 0] = angle * axis
-
-    if gains_in_end_effector_frame:
-      xyz_error[0:3] = arm_tip_fk[0:3, 0:3].T @ xyz_error.reshape((3, 1))
-      rot_error_vec[0:3] = arm_tip_fk[0:3, 0:3].T @ rot_error_vec
-
-    pos_error[0:3] = xyz_error.flat
-    pos_error[3:6] = rot_error_vec.flat
-    vel_error[0:6] = (J_arm_tip @ (arm_cmd_joint_vels - fbk.velocity).T).flat
-
-    # spring_wrench = spring_gains *. pos_error
-    spring_wrench[:, 0] = spring_gains * pos_error.flatten()
-
-    if gains_in_end_effector_frame:
-      spring_wrench[0:3] = arm_tip_fk[0:3, 0:3] @ spring_wrench[0:3]
-      spring_wrench[3:6] = arm_tip_fk[0:3, 0:3] @ spring_wrench[3:6]
-
-    # damper_wrench = damper_gains *. vel_error
-    damper_wrench[:, 0] = damper_gains * vel_error.flatten()
-
-    impedance_effort[:, 0] = (J_arm_tip.T @ (spring_wrench + damper_wrench)).T
-    grav_effort[:] = grav_effort + impedance_effort.flatten()
-    cmd.effort = grav_effort
-  else:
-    cmd.effort = grav_effort
-
-  # Send to robot
-  group.send_command(cmd)
-
-  # See if user requested mode toggle
+  # If in impedance mode set led blue
+  m.set_led_color("blue" if controller_on else "green")
   # If button 2 is pressed set to impedance mode
-  if m.get_button_state(2):
-    controller_on = True
-  else:
-    controller_on = False
+  controller_on = bool(m.get_button_state(2))
 
 m.set_led_color("red")
 
 if enable_logging:
-  hebi_log = group.stop_log()
+  hebi_log = arm.group.stop_log()
 
   # Plot tracking / error from the joints in the arm.  
   time = []
--- kits/arm/ex_mobile_io_control.py
+++ kits/arm/ex_mobile_io_control.py
@@ -4,32 +4,21 @@ import hebi
 import numpy as np
 import os
 import sys
-
-
-# ------------------------------------------------------------------------------
-# Add the root folder of the repository to the search path for modules
-root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-sys.path = [root_path] + sys.path
-# ------------------------------------------------------------------------------
-
-
-from util.math_utils import get_grav_comp_efforts
-from util.arm import setup_arm_params
-from time import sleep, time
-from util import math_utils
+from time import sleep
+from hebi import arm as arm_api
 from hebi.util import create_mobile_io
 
 run_mode = "points"
-
 first_run = True
 
-# Mobile device setup
 phone_family = 'HEBI'
 phone_name   = "mobileIO"
+arm_family   = "Example Arm"
 
 lookup = hebi.Lookup()
 sleep(2)
 
+# Setup MobileIO
 print('Waiting for Mobile IO device to come online...')
 m = create_mobile_io(lookup, phone_family, phone_name)
 if m is None:
@@ -39,172 +28,63 @@ m.set_button_mode(2, 'momentary')
 m.set_button_mode(3, 'momentary')
 m.update()
 
+# Setup arm components
+arm = arm_api.create(arm_family,
+                     lookup=lookup,
+                     hrdf_file=os.path.join(os.path.dirname(__file__), 'hrdf', 'A-2085-06.hrdf'))
+
+# Configure arm components
+# TODO
+
 quit_demo_button = 8
-abort_flag = False
-lookup = hebi.Lookup()
-sleep(2)
-
-# Arm Setup
-arm_family = 'Example Arm'
-arm_name   = '6-DoF'
-has_gas_spring = False # If you attach a gas spring to the shoulder for extra payload, set this to True.
-
-group, kin, params = setup_arm_params(arm_name, arm_family, has_gas_spring, lookup=lookup)
-
-fbk = hebi.GroupFeedback(group.size)
-effort_offset = params.effort_offset
-gravity_vec = params.gravity_vec
-local_dir = params.local_dir
-
-arm_dof_count = kin.dof_count
+keep_running = True
+point_1 = np.asarray([np.pi/4, np.pi/3, 2*np.pi/3, np.pi/3, np.pi/4, 0])
+point_2 = np.asarray([-np.pi/4, np.pi/3, 2*np.pi/3, np.pi/3, 3*np.pi/4, 0])
+point_3 = np.asarray([0, 0, 0, 0, 0, 0])
 
 print('B1-3 - select different points to move the arm to.')
 print('B6 - enables grav comp mode.')
 print('B8 - Quits the demo.')
                            
-cmd = hebi.GroupCommand(group.size)
+while keep_running:
 
-# Point coordinates
-point_1 = np.asarray([np.pi/4, np.pi/3, 2*np.pi/3, np.pi/3, np.pi/4, 0])
-point_2 = np.asarray([-np.pi/4, np.pi/3, 2*np.pi/3, np.pi/3, 3*np.pi/4, 0])
-point_3 = np.asarray([0, 0, 0, 0, 0, 0])
+  if not arm.update():
+    print("Failed to update arm")
+    continue
 
-end_velocities = np.zeros(arm_dof_count)
-end_accels = np.zeros(arm_dof_count)
-
-# Zero the times
-t = 0
-duration = 0
-
-time_vector = [0, 5]
-start = time()
-
-pos_cmd = np.zeros(group.size)
-vel_cmd = np.zeros(group.size)
-acc_cmd = np.zeros(group.size)
-
-
-def get_trajectory(curr_pos, curr_vel, curr_acc, point):
-  """
-  Helper function to create a trajectory
-  """
-  waypoints = np.empty((group.size, 2))
-  waypoints[:, 0] = curr_pos
-  waypoints[:, 1] = point
-
-  velocities = np.empty((group.size, 2))
-  velocities[:, 0] = curr_vel
-  velocities[:, 1] = end_velocities
-
-  accels = np.empty((group.size, 2))
-  accels[:, 0] = curr_acc
-  accels[:, 1] = end_accels
-
-  trajectory = hebi.trajectory.create_trajectory(time_vector, waypoints, velocities, accels)
-  return trajectory
-
-
-while not abort_flag:
-  # Update MobileIO state
   if not m.update():
     print("Failed to get feedback from MobileIO")
     continue
-
-  # Update arm state
-  group.get_next_feedback(reuse_fbk=fbk)
 
   # Check for quit
   if m.get_button_state(quit_demo_button):
     # Set led red and quit
     m.set_led_color("red")
-    abort_flag = False
+    keep_running = False
     break
 
   # On first run go to point 1
   if first_run:
-    run_mode = "points"
-    first_run = False
-    trajectory = get_trajectory(fbk.position, end_velocities, end_accels, point_1)
+    a.set_goal(point_1, times=[4])
 
-    duration = trajectory.duration
-    start = time()
-    t = time() - start
-
-  # If button 1 pressed, create trajectory from current position to point 1
+  # B1 point 1
   elif m.get_button_diff(1) == 3: # "ToOn"
-    run_mode = "points"
-    trajectory = get_trajectory(pos_cmd, vel_cmd, acc_cmd, point_1)
+    a.set_goal(point_1, times=[4])
 
-    duration = trajectory.duration
-    start = time()
-    t = time() - start
-
-  # If button 2 pressed, create trajectory from current position to point 2
+  # B2 point 2
   elif m.get_button_diff(2) == 3: # "ToOn"
-    run_mode = "points"
-    trajectory = get_trajectory(pos_cmd, vel_cmd, acc_cmd, point_2)
+    a.set_goal(point_2, times=[4])
 
-    duration = trajectory.duration
-    start = time()
-    t = time() - start
-
-  # If button 3 pressed, create trajectory from current position to point 3
+  # B3 point 3
   elif m.get_button_diff(3) == 3: # "ToOn"
-    run_mode = "points"
-    trajectory = get_trajectory(pos_cmd, vel_cmd, acc_cmd, point_3)
+    a.set_goal(point_3, times=[4])
 
-    duration = trajectory.duration
-    start = time()
-    t = time() - start
-
-  # If button 5 pressed switch to grav comp mode    
-  elif m.get_button_diff(5) == 3: # "ToOn"
+  # B6 grav comp
+  elif m.get_button_diff(6) == 3: # "ToOn"
     run_mode = "grav comp"
+    arm.cancel_goal()
 
-  if run_mode == "points":
-    # Set led to green for point move mode
-    m.set_led_color("green")
-    # Move to point
-    if t < duration:
-      t = time() - start
+  arm.send()
 
-      # Get new commands from the trajectory
-      pos_cmd, vel_cmd, acc_cmd = trajectory.get_state(t)
-
-      # Calculate commanded efforts to assist with tracking the trajectory.
-      # Gravity Compensation uses knowledge of the arm's kinematics and mass to
-      # compensate for the weight of the arm. Dynamic Compensation uses the
-      # kinematics and mass to compensate for the commanded accelerations of the arm.
-      eff_cmd = math_utils.get_grav_comp_efforts(kin, fbk.position, [0, 0, 1])
-      # NOTE: dynamic compensation effort computation has not yet been added to the APIs
-
-      # Fill in the command and send commands to the arm
-      cmd.position = pos_cmd
-      cmd.velocity = vel_cmd
-      cmd.effort = eff_cmd
-      group.send_command(cmd)
-    else:
-      cmd.position = pos_cmd
-      group.send_command(cmd)
-
-  elif run_mode == "grav comp":
-    # Set led to blue for grav comp mode
-    m.set_led_color("blue")
-    # Grav comp mode
-    # Update gravity vector the base module of the arm
-    params.update_gravity(fbk)
-    gravity_vec = params.gravity_vec
-
-    # Calculate required torques to negate gravity at current position
-    cmd.effort = get_grav_comp_efforts(kin, fbk.position, -gravity_vec) + effort_offset
-    nan_array = np.empty(group.size)
-    nan_array[:] = np.nan
-    cmd.position = nan_array
-    cmd.velocity = nan_array
-
-    # Update these so when creating next trajectory in point move mode they are current
-    pos_cmd = fbk.position
-    vel_cmd = fbk.velocity
-    acc_cmd = end_accels
-    # Send to robot
-    group.send_command(cmd)
+  # Set led to blue for grav comp mode, green for points mode
+  m.set_led_color("green" if (run_mode == "points") else "blue")
