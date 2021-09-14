c17fdb97696364d09099f640bc3108903d1b7541
Chris Bollinger <chris@hebirobotics.com>
2021-09-13 20:55:31 -0400

    First pass on moving "flipper lock" out of base class
    Needs more testing before full removal

--- kits/tready/tready.py
+++ kits/tready/tready.py
@@ -35,8 +35,6 @@ class TreadedBase:
         self.flipper_cmd = self.cmd.create_view([0, 1, 2, 3])
         self.wheel_cmd = self.cmd.create_view([4, 5, 6, 7])
 
-        self.joined_flipper_mode = False
-
         self.chassis_ramp_time = chassis_ramp_time
         self.flipper_ramp_time = flipper_ramp_time
 
@@ -47,24 +45,20 @@ class TreadedBase:
         self.chassis_traj = hebi.trajectory.create_trajectory(times, np.zeros((3, 2)))
         times = np.array([0.0, flipper_ramp_time]) + t_now
         self.flipper_traj = hebi.trajectory.create_trajectory(times, np.zeros((4, 2)))
-        self.flipper_pos_traj = None
 
         self.cmd.position = self.fbk.position
         self.t_prev = t_now
 
-    def trajectory_complete(self, t_now):
+    def has_active_trajectory(self, t_now):
         if self.chassis_traj is not None and t_now < self.chassis_traj.end_time:
-            return False
+            return True
         if self.flipper_traj is not None and t_now < self.flipper_traj.end_time:
-            return False
-        if self.flipper_pos_traj is not None and t_now < self.flipper_pos_traj.end_time:
-            return False
-        return True
+            return True
+        return False
 
-    def cancel_trajectory(self):
+    def clear_trajectories(self):
         self.chassis_traj = None
         self.flipper_traj = None
-        self.flipper_pos_traj = None
 
     @property
     def wheel_to_chassis_vel(self):
@@ -113,36 +107,20 @@ class TreadedBase:
     def update(self, t_now):
         dt = t_now - self.t_prev
 
-        # switch to position trajectory to align flippers if needed
-        if self.joined_flipper_mode and not self.flippers_aligned:
-            # this sets flipper_pos_traj, so flippers align
-            pos = np.array([self.flipper_cmd.position, self.aligned_flipper_position])
-            self.set_flipper_pos_trajectory(t_now, [0, 3.0], pos)
-
-        # if we are homing, need to set command differently
-        if self.flipper_pos_traj is None and self.flipper_traj is None and self.chassis_traj is None:
+        if self.flipper_traj is None and self.chassis_traj is None:
             self.cmd.velocity = 0.0
-        elif self.flipper_pos_traj is not None and t_now < self.flipper_pos_traj.end_time:
-            t = min(t_now, self.flipper_pos_traj.end_time)
-            [pos, vel, accel] = self.flipper_pos_traj.get_state(t)
-            self.flipper_cmd.position = pos
-            self.flipper_cmd.velocity = vel
-            #nan = np.empty((1, 4))
-            #nan.fill(np.nan)
-            self.wheel_cmd.position = np.nan
-            self.wheel_cmd.velocity = np.nan
         else:
             if self.chassis_traj is not None:
                 # chassis update
                 t = min(t_now, self.chassis_traj.end_time)
-                [vel, acc, jerk] = self.chassis_traj.get_state(t)
+                [pos, vel, acc] = self.chassis_traj.get_state(t)
                 self.wheel_cmd.velocity = self.chassis_to_wheel_vel @ vel
                 self.wheel_cmd.position += self.wheel_cmd.velocity * dt
 
             if self.flipper_traj is not None:
                 # flipper update
                 t = min(t_now, self.flipper_traj.end_time)
-                [vel, acc, jerk] = self.flipper_traj.get_state(t)
+                [pos, vel, eff] = self.flipper_traj.get_state(t)
                 self.flipper_cmd.velocity = vel
 
                 if self.joined_flipper_mode and self.flippers_aligned:
@@ -153,52 +131,45 @@ class TreadedBase:
         self.group.send_command(self.cmd)
         self.t_prev = t_now
 
-    def set_flipper_pos_trajectory(self, t_now, times, positions):
-        # when this is happening (homing) we don't use the other two trajectories
-        self.chassis_traj = None
-        self.flipper_traj = None
-        times = np.array(times, dtype=np.float64)
-        self.flipper_pos_traj = hebi.trajectory.create_trajectory(times + t_now, positions)
-
-    def set_flipper_vel_trajectory(self, t_now, velocities):
-        times = np.array([0, self.flipper_ramp_time], dtype=np.float64)
-        vels = np.empty((4, 2))
-        accs = np.empty((4, 2))
-        jerks = np.empty((4, 2))
+    def set_flipper_trajectory(self, t_now, ramp_time, p=None, v=None):
+        times = [t_now, t_now + ramp_time]
+        positions = np.empty((4, 2))
+        velocities = np.empty((4, 2))
+        efforts = np.empty((4, 2))
 
         if self.flipper_traj is not None:
             t = min(t_now, self.flipper_traj.end_time)
-            [vels[:, 0], accs[:, 0], jerks[:, 0]] = self.flipper_traj.get_state(t)
+            positions[:, 0], velocities[:, 0], efforts[:, 0] = self.flipper_traj.get_state(t)
         else:
-            vels[:, 0] = self.flipper_fbk.velocity
-            accs[:, 0] = self.flipper_fbk.effort
-            jerks[:, 0] = np.zeros(4)
+            positions[:, 0] = base.flipper_fbk.position
+            velocities[:, 0] = self.flipper_fbk.velocity
+            efforts[:, 0] = self.flipper_fbk.effort
 
-        vels[:, 1] = velocities
-        accs[:, 1] = np.zeros(4)
-        jerks[:, 1] = np.zeros(4)
+        positions[:, 1] = np.nan if p is None else p
+        velocities[:, 1] = 0.0 if v is None else v
+        efforts[:, 1] = 0.0
 
-        self.flipper_traj = hebi.trajectory.create_trajectory(times, vels, accs, jerks)
+        self.flipper_traj = hebi.trajectory.create_trajectory(times, positions, velocities, efforts)
 
-    def set_chassis_vel_trajectory(self, t_now, velocities):
-        times = np.array([0, self.chassis_ramp_time], dtype=np.float64)
-        vels = np.empty((3, 2))
-        accs = np.empty((3, 2))
-        jerks = np.empty((3, 2))
+    def set_chassis_vel_trajectory(self, t_now, ramp_time, v):
+        times = [t_now, t_now + ramp_time]
+        positions = np.empty((3, 2))
+        velocities = np.empty((3, 2))
+        efforts = np.empty((3, 2))
 
         if self.chassis_traj is not None:
-            t = min(t_now, self.chassis_traj.end_time)
-            [vels[:, 0], accs[:, 0], jerks[:, 0]] = self.chassis_traj.get_state(t)
+            t = min(t_now, self.flipper_traj.end_time)
+            positions[:, 0], velocities[:, 0], efforts[:, 0] = self.chassis_traj.get_state(t)
         else:
-            vels[:, 0] = self.wheel_to_chassis_vel @ self.wheel_fbk.velocity
-            accs[:, 0] = self.wheel_to_chassis_vel @ self.wheel_fbk.effort
-            jerks[:, 0] = np.zeros(3)
+            positions[:, 0] = 0.0
+            velocities[:, 0] = self.wheel_to_chassis_vel @ self.wheel_fbk.velocity
+            efforts[:, 0] = self.wheel_to_chassis_vel @ self.wheel_fbk.effort
 
-        vels[:, 1] = velocities
-        accs[:, 1] = np.zeros(3)
-        jerks[:, 1] = np.zeros(3)
+        positions[:, 1] = np.nan
+        velocities[:, 1] = v
+        efforts[:, 1] = 0.0
 
-        self.chassis_traj = hebi.trajectory.create_trajectory(times, vels, accs, jerks)
+        self.chassis_traj = hebi.trajectory.create_trajectory(times, positions, velocities, efforts)
 
     def set_color(self, color):
         color_cmd = hebi.GroupCommand(self.group.size)
@@ -215,6 +186,7 @@ class TreadyState(Enum):
     STARTUP = auto()
     HOMING = auto()
     TELEOP = auto()
+    ALIGN_FLIPPERS = auto()
     DISCONNECTED = auto()
     EXIT = auto()
 
@@ -227,8 +199,8 @@ class TreadyControl:
         self.state = TreadyState(TreadyState.STARTUP)
         self.base = base
         self.mobile_io = mobile_io
-        self.SPEED_MAX_LIN = 0.125 # m/s
-        self.SPEED_MAX_ROT = self.SPEED_MAX_LIN / base.WHEEL_BASE # rad/s
+        self.SPEED_MAX_LIN = 0.125  # m/s
+        self.SPEED_MAX_ROT = self.SPEED_MAX_LIN / base.WHEEL_BASE  # rad/s
 
     def process_mobile_input(self, t_now):
         reset_pose_btn = 1
@@ -243,10 +215,24 @@ class TreadyControl:
         joy_fwd = 2
         joy_rot = 1
 
+        aligned_flipper_mode = self.mobile_io.get_button_state(joined_flipper_btn)
+
+        ret_val = {
+            'change_state': None,
+            'flipper_pos': None,
+            'flipper_vel': None,
+            'chassis_vel': None
+        }
+
         if self.mobile_io.get_button_state(quit_btn) == 1:
-            self.transition_to(TreadyState.EXIT)
+            ret_val['change_state'] = TreadyState.EXIT
+            return ret_val
         elif self.mobile_io.get_button_state(reset_pose_btn) == 1:
-            self.transition_to(TreadyState.HOMING)
+            ret_val['change_state'] = TreadyState.HOMING
+            return ret_val
+        elif aligned_flipper_mode and not self.base.flippers_aligned:
+            ret_val['change_state'] = TreadyState.ALIGN_FLIPPERS
+            return ret_val
 
         # Flipper Control
         flip1 = self.mobile_io.get_axis_state(slider_flip1)
@@ -254,22 +240,21 @@ class TreadyControl:
         flip3 = self.mobile_io.get_axis_state(slider_flip3)
         flip4 = self.mobile_io.get_axis_state(slider_flip4)
 
-        if self.mobile_io.get_button_state(joined_flipper_btn):
-            self.base.joined_flipper_mode = True
+        if aligned_flipper_mode:
             f_vel1 = max(abs(flip1), abs(flip2)) * np.sign(flip1 + flip2) * self.FLIPPER_VEL_SCALE
             f_vel2 = -f_vel1
             f_vel3 = max(abs(flip3), abs(flip4)) * np.sign(flip3 + flip4) * self.FLIPPER_VEL_SCALE
             f_vel4 = -f_vel3
 
+            ret_val['flipper_pos'] = np.repeat(np.nan, 4)
+
         else:
-            self.base.joined_flipper_mode = False
             f_vel1 = flip1 * self.FLIPPER_VEL_SCALE
             f_vel2 = -1 * flip2 * self.FLIPPER_VEL_SCALE
             f_vel3 = flip3 * self.FLIPPER_VEL_SCALE
             f_vel4 = -1 * flip4 * self.FLIPPER_VEL_SCALE
 
-        flipper_vels = [f_vel1, f_vel2, f_vel3, f_vel4]
-        base.set_flipper_vel_trajectory(t_now, flipper_vels)
+        ret_val['flipper_vel'] = [f_vel1, f_vel2, f_vel3, f_vel4]
 
         # Mobile Base Control
         joy_vel_fwd = self.mobile_io.get_axis_state(joy_fwd)
@@ -279,8 +264,9 @@ class TreadyControl:
         vel_y = 0
         vel_rot = self.SPEED_MAX_ROT * joy_vel_rot
 
-        chassis_vels = [vel_x, vel_y, vel_rot]
-        base.set_chassis_vel_trajectory(t_now, chassis_vels)
+        ret_val['chassis_vel'] = [vel_x, vel_y, vel_rot]
+
+        return None, chassis_vels, flipper_vels
 
     def update(self, t_now):
         has_mobile_feedback = self.mobile_io.update(0.0)
@@ -299,30 +285,40 @@ class TreadyControl:
 
             self.mobile_io.set_button_output(1, 1)
             self.mobile_io.set_button_output(8, 1)
-            self.transition_to(TreadyState.HOMING)
+            self.transition_to(t_now, TreadyState.HOMING)
 
         elif self.state is TreadyState.HOMING:
-            if base.trajectory_complete(t_now):
-                self.transition_to(TreadyState.TELEOP)
+            if not base.has_active_trajectory(t_now):
+                self.transition_to(t_now, TreadyState.TELEOP)
+
+        elif self.state is TreadyState.ALIGN_FLIPPERS:
+            if not base.has_active_trajectory(t_now):
+                self.transition_to(t_now, TreadyState.TELEOP)
 
         elif self.state is TreadyState.TELEOP:
             if not has_mobile_feedback:
                 if t_now - self.mobile_last_fbk_t > 1.0:
                     print("mobileIO timeout, disabling motion")
-                    self.transition_to(TreadyState.DISCONNECTED)
+                    self.transition_to(t_now, TreadyState.DISCONNECTED)
             else:
-                self.process_mobile_input(t_now)
+                next_state, chassis_vels, flipper_vels = self.process_mobile_input(t_now)
+                if next_state is not None:
+                    self.transition_to(t_now, next_state)
+                else:
+                    self.base.set_chassis_vel_trajectory(t_now, self.chassis_ramp_time, chassis_vels)
+                    self.base.set_flipper_trajectory(t_now, self.flipper_ramp_time, v=flipper_vels)
 
         elif self.state is TreadyState.DISCONNECTED:
             if has_mobile_feedback:
                 self.mobile_last_fbk_t = t_now
-                self.transition_to(TreadyState.TELEOP)
+                self.transition_to(t_now, TreadyState.TELEOP)
 
         elif self.state is TreadyState.EXIT:
             return False
+
         return True
 
-    def transition_to(self, state):
+    def transition_to(self, t_now, state):
         if state is TreadyState.HOMING:
             print("TRANSITIONING TO HOMING")
             base.set_color('magenta')
@@ -331,14 +327,27 @@ class TreadyControl:
             m.set_text(msg)
 
             # build trajectory
-            flipper_positions = np.empty((4, 2))
-            if self.state is TreadyState.STARTUP:
-                flipper_positions[:, 0] = base.flipper_fbk.position
-            else:
-                flipper_positions[:, 0] = base.flipper_fbk.position_command
-            flipper_positions[:, 1] = np.deg2rad([-15, 15, 15, -15])
+            flipper_home = np.deg2rad([-15, 15, 15, -15])
+            times = np.array([0.0, 5.0]) + t_now
 
-            base.set_flipper_pos_trajectory(time(), [0.0, 5.0], flipper_positions)
+            if self.state is TreadyState.STARTUP:
+                flipper_positions = np.empty((4, 2))
+                flipper_positions[:, 0] = base.flipper_fbk.position
+                flipper_positions[:, 1] =
+                self.flipper_pos_traj = hebi.trajectory.create_trajectory(times, flipper_positions)
+            else:
+                base.set_flipper_trajectory(t_now, 5.0, p=flipper_home)
+
+        elif state is TreadyState.ALIGN_FLIPPERS:
+            print("ALIGNING FLIPPERS")
+            m.clear_text()
+            msg = 'Aligning Flippers\nPlease wait...'
+            m.set_text(msg)
+            self.base.clear_trajectories()
+            # this sets flipper_pos_traj, so flippers align
+            pos = [self.base.flipper_fbk.position_command, self.base.aligned_flipper_position]
+            self.base.set_flipper_trajectory(t_now, 3.0, p=pos)
+
         elif state is TreadyState.TELEOP:
             print("TRANSITIONING TO TELEOP")
             base.clear_color()
@@ -347,10 +356,12 @@ class TreadyControl:
             instructions = 'Robot Ready to Control\nB1: Reset\nB6: Joined Flipper\nB8 - Quit'
             m.set_text(instructions)
             m.set_led_color("green")
+
         elif state is TreadyState.DISCONNECTED:
             print("TRANSITIONING TO DISCONNECTED")
-            base.cancel_trajectory()
+            base.clear_trajectories()
             base.set_color('blue')
+
         elif state is TreadyState.EXIT:
             print("TRANSITIONING TO EXIT")
             # unset mobileIO control config
@@ -362,6 +373,7 @@ class TreadyControl:
             self.mobile_io.set_button_output(8, 0)
             m.clear_text()
             m.set_text('Demo Stopped.')
+
         self.state = state
 
 
