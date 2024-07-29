1f2586ef47edae2a02f5aea51450a31a3fc261ae
Aditya Nair <59332714+GogiPuttar@users.noreply.github.com>
2024-07-29 11:51:42 -0400

    Gogiputtar/fix impedance ex (#75)
    * Fixed cartesian gain values
    Fixed mean position drift error in impedance controller
    Added emojis to mobileIO buttons
    
    * Complete example for springy impedance control with 4 demos.
    
    * Added multiple demos to the hybrid impedance controller example
    
    * Improved documentation
    
    * Increased floor stiffness
    
    * Created a rope example using impedance control plugin that does not behave exactly like a rope.
    Trying to use just force control to get this behaviour
    
    * Functional force control rope example
    
    * Functional zonal weights example
    
    * Hybrid-motion controllers using impedance control are ready to use
    
    * moved all hybrid controller examples directly to default impedance controller examples file
    
    * got rid of unimportant examples
    
    * Fixed typos in documentation
    
    * Temporarily removed force control examples to create PR
    
    * restored force control example after MR
    
    * Deleted force control examples again for making changes to PR
    Split impedance controller examples into 4 different files
    
    * Properly incorporated integral gains into examples
    Removed unnecessary gains resetting in floor example
    
    * Added complete damping example
    
    * Added comment explaining i_clamp
    
    * Used the spread operator
    
    * Incorporated separate script for plotting
    Revised imports
    Standardized arm families across examples

---
+++ kits/arm/ex_impedance_control_damping.py
@@ -0,0 +1,184 @@
+"""
+In this example we will implement various hybrid motion-force controllers using the impedance control plugin, which can be used for a wide variety of 
+applications.
+Impedance control is BEST SUITED for enabling free, rigid and springy behaviour, along/about each different axis.
+While this is perfectly useful for:
+- Having a selectively compliant end-effector,
+- Switching between fixed and free behaviour to simulate (mostly) rigid constraints, and
+- Allowing human intervention for automated operations by separating controls across different axes,
+any applications involving more salient control of the forces (as more complex functions with flexible inputs) should use our force control plugin. See ex_force_control_demoname.py.
+
+This comprises the following demos:
+- Fixed: A task-space pose controller implemented entirely using force control via the (PD) impedance controller.
+- Cartesian: Locks onto a particular end-effector position while having some compliant orientation.
+- Gimbal: A gimbal that locks a specific end-effector orientation, while keeping the rest of the arm compliant.
+- Floor: The end-effector is free to move but can't travel below a virtual floor. To further simulate sliding on the floor, see force_control example.
+- Damping: The end-effector behaves as 3-different damped systems (overdamped, critically damped, and underdamped), at 3 different heights.
+
+The following example is for the "Damping" demo:
+"""
+#!/usr/bin/env python3
+
+import hebi
+from time import sleep
+from hebi.util import create_mobile_io
+import numpy as np
+from plotting import draw_plots
+
+# NOTE: Angle wraparound is an unresolved issue which can lead to unstable behaviour for any case involving rotational positional control. 
+#       Make sure that the rotational gains are high enough to prevent large angular errors. The gains provided in these examples are (mostly) well behaved.
+#       Interacting with the end-effector in these examples is perfectly safe.
+#       However, ensure that nothing prevents the wrist's actuators from moving, and DO NOT place your fingers between them. 
+
+# Initialize the interface for network connected modules
+lookup = hebi.Lookup()
+sleep(2)
+
+# Set up arm
+phone_family = "Arm"
+phone_name = "mobileIO"
+arm_family = "Arm"
+hrdf_file = "hrdf/A-2085-06.hrdf"
+gains_file = "gains/A-2085-06.xml"
+
+# Set up Mobile IO
+print('Waiting for Mobile IO device to come online...')
+m = create_mobile_io(lookup, phone_family, phone_name)
+if m is None:
+    raise RuntimeError("Could not find Mobile IO device")
+m.set_button_mode(1, 'momentary')
+m.set_button_label(1, '📈')
+m.set_button_mode(2, 'toggle')
+m.set_button_label(2, '💪')
+m.update()
+
+# Setup arm components
+arm = hebi.arm.create([arm_family],
+                      names=['J1_base', 'J2_shoulder', 'J3_elbow', 'J4_wrist1', 'J5_wrist2', 'J6_wrist3'],
+                      lookup=lookup,
+                      hrdf_file=hrdf_file)
+arm.load_gains(gains_file)
+
+impedance_controller = hebi.arm.ImpedanceController()
+
+# Clear all position control gains for all the actuators
+cmd = arm.pending_command
+
+cmd.position_kp = 0.0
+cmd.position_kd = 0.0
+cmd.position_ki = 0.0
+
+# Configure arm components
+arm.add_plugin(impedance_controller)
+
+# No need to set gains initially
+
+# Keep in end-effector frame since it is more intuitive to define rotational stiffness
+impedance_controller.gains_in_end_effector_frame = False
+
+# Meters above the base for overdamped, critically damped, and underdamped cases respectively
+lower_limits = [0.0, 0.15, 0.30]
+# State variable for current mode: 0 for overdamped, 1 for crtically damped, 2 for underdamped, -1 for free
+mode = -1
+prevmode = -1
+
+# 0: Overdamped
+overdamped_kp = np.array([100, 100, 0, 5, 5, 1])
+overdamped_kd = np.array([15, 15, 0, 0, 0, 0])
+
+# 1: Critically damped
+critically_damped_kp = np.array([100, 100, 0, 5, 5, 1])
+critically_damped_kd = np.array([5, 5, 0, 0, 0, 0])
+
+# 2: Underdamped
+underdamped_kp = np.array([100, 100, 0, 5, 5, 1])
+underdamped_kd = np.array([0, 0, 0, 0, 0, 0])
+
+damping_kp = [overdamped_kp, critically_damped_kp, underdamped_kp]
+damping_kd = [overdamped_kd, critically_damped_kd, underdamped_kd]
+
+# Increase feedback frequency since we're calculating velocities at the
+# high level for damping. Going faster can help reduce a little bit of
+# jitter for fast motions, but going slower (100 Hz) also works just fine
+# for most applications.
+arm.group.feedback_frequency = 200.0
+
+# Double the effort gains from their default values, to make the arm more sensitive for tracking force.
+# TODO
+
+enable_logging = True
+goal = hebi.arm.Goal(arm.size)
+
+# Start background logging
+if enable_logging:
+    arm.group.start_log('dir', 'logs', mkdirs=True)
+
+print('Commanded gravity-compensated zero force to the arm.')
+print('  💪 (B2) - Toggles an impedance controller on/off:')
+print('            ON  - Apply controller based on current position')
+print('            OFF - Go back to gravity-compensated mode')
+print('  📈 (B1) - Exits the demo, and plots graphs. May take a while.')
+
+controller_on = False
+
+# while button 1 is not pressed
+while not m.get_button_state(1):
+
+    # if not arm.update():
+    #     print("Failed to update arm")
+    #     continue
+    arm.update()
+
+    arm.send()
+
+    if m.update(timeout_ms=0):
+
+        # Set and unset impedance mode when button is pressed and released, respectively
+        if (m.get_button_diff(2) == 1):
+
+            controller_on = True
+            arm.set_goal(goal.clear().add_waypoint(position=arm.last_feedback.position))
+
+        elif (m.get_button_diff(2) == -1):
+            
+            controller_on = False
+
+        # If in impedance mode set led blue
+        m.set_led_color("blue" if controller_on else "green", blocking=False)
+
+    if not controller_on:
+        arm.cancel_goal()
+        mode = -1 # Free
+        prevmode = -1
+
+    # Check when end-effector travels below the floor, for floor demo
+    else:
+
+        # Use forward kinematics to calculate pose of end-effector
+        ee_pose_curr = np.eye(4)
+        arm.robot_model.get_end_effector(arm.last_feedback.position, ee_pose_curr)
+
+        # Assign mode based on current position
+        for i in range(len(lower_limits)):
+
+            if ee_pose_curr[2,3] > lower_limits[i]:
+
+                mode = i
+        
+        # Change gains only upon mode switches
+        if not mode == prevmode and mode >= 0:
+
+            impedance_controller.set_kp(*damping_kp[mode])
+            impedance_controller.set_kd(*damping_kd[mode])
+
+            print(f"Mode: {mode}")
+
+        prevmode = mode
+
+m.set_led_color("red")
+
+if enable_logging:
+    hebi_log = arm.group.stop_log()
+    draw_plots(hebi_log)
+
+    # Put more plotting code here
---
+++ kits/arm/ex_impedance_control_fixed.py
@@ -0,0 +1,141 @@
+"""
+In these examples we will implement various hybrid motion-force controllers using the impedance control plugin, which can be used for a wide variety of 
+applications.
+Impedance control is BEST SUITED for enabling free, rigid and springy behaviour, along/about each different axis.
+While this is perfectly useful for:
+- Having a selectively compliant end-effector,
+- Switching between fixed and free behaviour to simulate (mostly) rigid constraints, and
+- Allowing human intervention for automated operations by separating controls across different axes,
+any applications involving more salient control of the forces (as more complex functions with flexible inputs) should use our force control plugin. See ex_force_control_demoname.py.
+
+This comprises the following demos:
+- Fixed: A task-space pose controller implemented entirely using force control via the (PD) impedance controller.
+- Cartesian: Locks onto a particular end-effector position while having some compliant orientation.
+- Gimbal: A gimbal that locks a specific end-effector orientation, while keeping the rest of the arm compliant.
+- Floor: The end-effector is free to move but can't travel below a virtual floor. To further simulate sliding on the floor, see force_control example.
+- Damping: The end-effector behaves as 3-different damped systems (overdamped, critically damped, and underdamped), at 3 different heights.
+
+The following example is for the "Fixed" demo:
+"""
+#!/usr/bin/env python3
+
+import hebi
+from time import sleep
+from hebi.util import create_mobile_io
+import numpy as np
+from plotting import draw_plots
+
+# NOTE: Angle wraparound is an unresolved issue which can lead to unstable behaviour for any case involving rotational positional control. 
+#       Make sure that the rotational gains are high enough to prevent large angular errors. The gains provided in these examples are (mostly) well behaved.
+#       Interacting with the end-effector in these examples is perfectly safe.
+#       However, ensure that nothing prevents the wrist's actuators from moving, and DO NOT place your fingers between them. 
+
+# Initialize the interface for network connected modules
+lookup = hebi.Lookup()
+sleep(2)
+
+# Set up arm
+phone_family = "Arm"
+phone_name = "mobileIO"
+arm_family = "Arm"
+hrdf_file = "hrdf/A-2085-06.hrdf"
+gains_file = "gains/A-2085-06.xml"
+
+# Set up Mobile IO
+print('Waiting for Mobile IO device to come online...')
+m = create_mobile_io(lookup, phone_family, phone_name)
+if m is None:
+    raise RuntimeError("Could not find Mobile IO device")
+m.set_button_mode(1, 'momentary')
+m.set_button_label(1, '📈')
+m.set_button_mode(2, 'toggle')
+m.set_button_label(2, '💪')
+m.update()
+
+# Setup arm components
+arm = hebi.arm.create([arm_family],
+                      names=['J1_base', 'J2_shoulder', 'J3_elbow', 'J4_wrist1', 'J5_wrist2', 'J6_wrist3'],
+                      lookup=lookup,
+                      hrdf_file=hrdf_file)
+arm.load_gains(gains_file)
+
+impedance_controller = hebi.arm.ImpedanceController()
+
+# Clear all position control gains for all the actuators
+cmd = arm.pending_command
+
+cmd.position_kp = 0.0
+cmd.position_kd = 0.0
+cmd.position_ki = 0.0
+
+# Configure arm components
+arm.add_plugin(impedance_controller)
+
+# Dictate impedance controller gains in SE(3) based on the demo
+impedance_controller.set_kp(300, 300, 300, 5, 5, 1)
+impedance_controller.set_kd(5, 5, 5, 0, 0, 0)
+impedance_controller.set_ki(20, 20, 20, 0.5, 0.5, 0.5)
+impedance_controller.set_i_clamp(10, 10, 10, 1, 1, 1) # Clamp on the end-effector wrench and NOT on the integral error
+
+# Keep in end-effector frame since it is more intuitive to define rotational stiffness
+impedance_controller.gains_in_end_effector_frame = True
+
+# Increase feedback frequency since we're calculating velocities at the
+# high level for damping. Going faster can help reduce a little bit of
+# jitter for fast motions, but going slower (100 Hz) also works just fine
+# for most applications.
+arm.group.feedback_frequency = 200.0
+
+# Double the effort gains from their default values, to make the arm more sensitive for tracking force.
+# TODO
+
+enable_logging = True
+goal = hebi.arm.Goal(arm.size)
+
+# Start background logging
+if enable_logging:
+    arm.group.start_log('dir', 'logs', mkdirs=True)
+
+print('Commanded gravity-compensated zero force to the arm.')
+print('  💪 (B2) - Toggles an impedance controller on/off:')
+print('            ON  - Apply controller based on current position')
+print('            OFF - Go back to gravity-compensated mode')
+print('  📈 (B1) - Exits the demo, and plots graphs. May take a while.')
+
+controller_on = False
+
+# while button 1 is not pressed
+while not m.get_button_state(1):
+
+    # if not arm.update():
+    #     print("Failed to update arm")
+    #     continue
+    arm.update()
+
+    arm.send()
+
+    if m.update(timeout_ms=0):
+
+        # Set and unset impedance mode when button is pressed and released, respectively
+        if (m.get_button_diff(2) == 1):
+
+            controller_on = True
+            arm.set_goal(goal.clear().add_waypoint(position=arm.last_feedback.position))
+
+        elif (m.get_button_diff(2) == -1):
+            
+            controller_on = False
+
+        # If in impedance mode set led blue
+        m.set_led_color("blue" if controller_on else "green", blocking=False)
+
+    if not controller_on:
+        arm.cancel_goal()
+
+m.set_led_color("red")
+
+if enable_logging:
+    hebi_log = arm.group.stop_log()
+    draw_plots(hebi_log)
+
+    # Put more plotting code here
