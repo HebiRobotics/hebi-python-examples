5f5a947b379bc3338befca2612615e0a06f88cb7
Aditya Nair <aditya.nair0123@gmail.com>
2024-08-06 09:15:43 -0400

    First pass

---
+++ kits/arm/glass_balancing.py
@@ -0,0 +1,129 @@
+"""
+In this example we will implement various hybrid motion-force controllers using the impedance control plugin, which can be used for a wide variety of 
+applications.
+Impedance control is BEST SUITED for enabling free, rigid and springy behaviour, along/about each different axis.
+While this is perfectly useful for:
+- Having a selectively compliant end-effector,
+- Switching between fixed and free behaviour to simulate (mostly) rigid constraints, and
+- Allowing human intervention for automated operations by separating controls across different axes,
+any applications involving more salient control of the forces (as more complex functions with flexible inputs) should use our force control plugin. See ex_force_control_demoname.py.
+
+This comprises the following demos:
+- Fixed: A task-space pose controller implemented entirely using force control via the (PD) impedance controller.
+- Cartesian: Locks onto a particular end-effector position while having some compliant orientation.
+- Gimbal: A gimbal that locks a specific end-effector orientation, while keeping the rest of the arm compliant.
+- Floor: The end-effector is free to move but can't travel below a virtual floor. To further simulate sliding on the floor, see force_control example.
+- Damping: The end-effector behaves as 3-different damped systems (overdamped, critically damped, and underdamped), at 3 different heights.
+
+The following example is for the "Gimbal" demo:
+"""
+#!/usr/bin/env python3
+
+import hebi
+from time import sleep
+from hebi.util import create_mobile_io_from_config, create_gripper_from_config
+import numpy as np
+from plotting import draw_plots
+
+# NOTE: Angle wraparound is an unresolved issue which can lead to unstable behaviour for any case involving rotational positional control. 
+#       Make sure that the rotational gains are high enough to prevent large angular errors. The gains provided in these examples are (mostly) well behaved.
+#       Interacting with the end-effector in these examples is perfectly safe.
+#       However, ensure that nothing prevents the wrist's actuators from moving, and DO NOT place your fingers between them. 
+
+# Initialize the interface for network connected modules
+lookup = hebi.Lookup()
+sleep(2)
+
+# Config file
+example_config_file = "config/examples/glass_balancing.cfg"
+
+# Set up arm from config
+example_config = hebi.config.load_config(example_config_file)
+arm = hebi.arm.create_from_config(example_config)
+
+# Set up Mobile IO from config
+print('Waiting for Mobile IO device to come online...')
+m = create_mobile_io_from_config(lookup, example_config)
+m.set_button_mode(2, 'toggle')
+m.update()
+
+# Add the gripper
+gripper = create_gripper_from_config(lookup, example_config)
+arm.set_end_effector(gripper)
+
+# Set up arm configuration
+example_config = hebi.config.load_config(example_config_file)
+arm = hebi.arm.create_from_config(example_config)
+
+# Clear all position control gains for all the actuators
+cmd = arm.pending_command
+
+cmd.position_kp = 0.0
+cmd.position_kd = 0.0
+cmd.position_ki = 0.0
+
+# Increase feedback frequency since we're calculating velocities at the
+# high level for damping. Going faster can help reduce a little bit of
+# jitter for fast motions, but going slower (100 Hz) also works just fine
+# for most applications.
+arm.group.feedback_frequency = 200.0
+
+# Double the effort gains from their default values, to make the arm more sensitive for tracking force.
+# TODO
+
+enable_logging = True
+goal = hebi.arm.Goal(arm.size)
+
+# Start background logging
+if enable_logging:
+    arm.group.start_log('dir', 'logs', mkdirs=True)
+
+print('Commanded gravity-compensated zero force to the arm.')
+print('  💪 (B2) - Toggles an impedance controller on/off:')
+print('            ON  - Apply controller based on current position')
+print('            OFF - Go back to gravity-compensated mode')
+print('  📈 (B1) - Exits the demo, and plots graphs. May take a while.')
+
+controller_on = False
+gripper_state = False
+
+# while button 1 is not pressed
+while not m.get_button_state(1):
+
+    # if not arm.update():
+    #     print("Failed to update arm")
+    #     continue
+    arm.update()
+
+    arm.send()
+
+    if m.update(timeout_ms=0):
+
+        # Set and unset impedance mode when button is pressed and released, respectively
+        if (m.get_button_diff(2) == 1):
+
+            controller_on = True
+            arm.set_goal(goal.clear().add_waypoint(position=arm.last_feedback.position))
+
+        elif (m.get_button_diff(2) == -1):
+            
+            controller_on = False
+
+        if (m.get_button_diff(3) == 1):
+
+            gripper_state = not gripper_state
+            gripper.toggle()
+
+        # If in impedance mode set led blue
+        m.set_led_color("blue" if controller_on else "green", blocking=False)
+
+    if not controller_on:
+        arm.cancel_goal()
+
+m.set_led_color("red")
+
+if enable_logging:
+    hebi_log = arm.group.stop_log()
+    draw_plots(hebi_log)
+
+    # Put more plotting code here
